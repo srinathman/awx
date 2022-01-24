f974b77bce23fdfe7d0fb4b3b163364b729cc4db
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2022-01-24 15:44:43 -0500

    Allows isolated jobs paths to be mounted k8s/ocp as hostPath volume

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -994,3 +994,6 @@ DEFAULT_CONTROL_PLANE_QUEUE_NAME = 'controlplane'
 # For example, to disable SELinux in containers for podman
 # DEFAULT_CONTAINER_RUN_OPTIONS = ['--security-opt', 'label=disable']
 DEFAULT_CONTAINER_RUN_OPTIONS = []
+
+# Mount exposed paths as hostPath resource in k8s/ocp
+AWX_MOUNT_ISOLATED_PATHS_ON_K8S = False
